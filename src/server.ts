685f420c7e3ab3ddae2519b5f5b29cbf32c61c26
erik-per-lidstrom <kirelidstrom@gmail.com>
2026-02-18 13:43:04 +0100

    added pool

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 // src/server.ts
 import dotenv from "dotenv";
 import { createApp } from "./app";
+import { pool } from "./config/db";
 
 dotenv.config();
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 4000;
 
 const app = createApp();
+
+// src/server.ts
+pool
+  .connect()
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.error("DB Error", err));
 
 app.listen(PORT, () => {
   console.log(`• Server running on http://localhost:${PORT}`);
